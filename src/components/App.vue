fb14c285a35fd8fd24b23d6c50aaf1d753bb3f46
vladimirborja <vladimirborja013@gmail.com>
2026-06-14 21:48:39 +0800

    chore: remove default Vue scaffold components

---
+++ src/components/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import NavBar from './components/NavBar.vue'
+import HeroSection from './components/HeroSection.vue'
+import SkillsSection from './components/SkillsSection.vue'
+import ProjectsSection from './components/ProjectsSection.vue'
+import ContactSection from './components/ContactSection.vue'
+</script>
+
+<template>
+    <div class="bg-[#0a0a0a] min-h-screen">
+        <NavBar />
+        <main>
+            <HeroSection />
+            <SkillsSection />
+            <ProjectsSection />
+            <ContactSection />
+        </main>
+    </div>
+</template>
